a941f58d20ee1bb5627b75d59669e07788c14072
Vasiliy Borisov <borisov1983@gmail.com>
2024-11-18 14:12:00 +0900

    Убрал длину поля slug модели AbstractTagModel

--- api_yamdb/reviews/abstracts.py
+++ api_yamdb/reviews/abstracts.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from reviews.constants import (NAME_MAX_LENGTH, SLUG_MAX_LENGTH,
+from reviews.constants import (NAME_MAX_LENGTH,
                                STR_OUTPUT_LIMIT)
 
 
@@ -8,7 +8,6 @@ class AbstractTagModel(models.Model):
     name = models.CharField('Название', max_length=NAME_MAX_LENGTH)
     slug = models.SlugField(
         'Человекочитаемый ключ',
-        max_length=SLUG_MAX_LENGTH,
         unique=True)
 
     class Meta:
