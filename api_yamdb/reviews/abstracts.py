040d25d4d723b895166d9e037a280ab8117f92ac
Vasiliy Borisov <borisov1983@gmail.com>
2024-11-18 13:56:22 +0900

    Починил модели

--- api_yamdb/reviews/abstracts.py
+++ api_yamdb/reviews/abstracts.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 from reviews.constants import (NAME_MAX_LENGTH, SLUG_MAX_LENGTH,
                                STR_OUTPUT_LIMIT)
 
@@ -13,6 +14,27 @@ class AbstractTagModel(models.Model):
 
     class Meta:
         abstract = True
+        ordering = ('name',)
 
     def __str__(self):
         return self.name[:STR_OUTPUT_LIMIT]
+
+
+class AbstractCommentReviewModel(models.Model):
+    author = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+        verbose_name='Автор'
+    )
+    pub_date = models.DateTimeField(
+        'Дата добавления',
+        auto_now_add=True,
+        db_index=True
+    )
+
+    class Meta:
+        abstract = True
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return f'Автор: {self.author}, Дата: {self.pub_date}'
--- api_yamdb/reviews/constants.py
+++ api_yamdb/reviews/constants.py
@@ -3,6 +3,10 @@ from typing import Final
 STR_OUTPUT_LIMIT: Final[int] = 30
 NAME_MAX_LENGTH: Final[int] = 256
 SLUG_MAX_LENGTH: Final[int] = 50
+FIELD_MAX_LENGTH: Final[int] = 254
+CONFIRMATION_CODE_MAX_LENGTH: Final[int] = 40
+DEFAULT_ROLE: Final[str] = 'user'
+ROLE_INDEX: Final[int] = 0
 
 ROLES = (
     ('user', 'user'),
