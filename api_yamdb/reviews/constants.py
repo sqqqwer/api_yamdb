6f9a3bfb6a8f3fd2008938dc924424f37c3671a0
Vasiliy Borisov <borisov1983@gmail.com>
2024-11-18 14:34:06 +0900

    Добавлены константы в кортеж ROLES в constants.py

--- api_yamdb/reviews/constants.py
+++ api_yamdb/reviews/constants.py
@@ -5,11 +5,15 @@ NAME_MAX_LENGTH: Final[int] = 256
 SLUG_MAX_LENGTH: Final[int] = 50
 FIELD_MAX_LENGTH: Final[int] = 254
 CONFIRMATION_CODE_MAX_LENGTH: Final[int] = 40
-DEFAULT_ROLE: Final[str] = 'user'
 ROLE_INDEX: Final[int] = 0
+DEFAULT_ROLE: Final[str] = 'user'
+ROLE_USER: Final[str] = 'user'
+ROLE_MODERATOR: Final[str] = 'moderator'
+ROLE_ADMIN: Final[str] = 'admin'
+
 
 ROLES = (
-    ('user', 'user'),
-    ('moderator', 'moderator'),
-    ('admin', 'admin')
+    (ROLE_USER, 'user'),
+    (ROLE_MODERATOR, 'moderator'),
+    (ROLE_ADMIN, 'admin')
 )
